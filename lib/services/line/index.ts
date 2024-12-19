671ca93d42c07d6ba9e7d80f3d2a4f28fd62394e
21 Tech Enterprise <59superrichsol@gmail.com>
2024-12-19 15:57:52 +0700

    fix

--- lib/services/line/index.ts
+++ lib/services/line/index.ts
@@ -6,8 +6,7 @@ export * from './profile';
 export * from './image';
 export * from './account';
 
-// Export commonly used functions directly
-export { getLineClient } from './client';
+
 export { sendLineMessage } from './message/send';
 export { processWebhookEvents } from './webhook/process';
 export { getLineUserProfile } from './profile/get';
