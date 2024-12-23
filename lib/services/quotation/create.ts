0066b83989cb466e9c13cff01af97ae7adfcec55
21 Tech Enterprise <59superrichsol@gmail.com>
2024-12-24 00:00:52 +0700

    try global prisma

--- lib/services/quotation/create.ts
+++ lib/services/quotation/create.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from "@/lib/db";
 import { QuotationCreateParams, QuotationResult } from './types';
 
-const prisma = new PrismaClient();
+
 
 export async function createQuotation(params: QuotationCreateParams): Promise<QuotationResult> {
   try {
